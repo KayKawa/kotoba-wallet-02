d7804522ca7e255715aec7ba94dff4bace5be1f3
KayKawa <goodenoughliving1221@gmail.com>
2021-10-11 04:10:38 +0900

    buyアソシエーション

--- app/models/buy.rb
+++ app/models/buy.rb
@@ -1,2 +1,7 @@
 class Buy < ApplicationRecord
+  belongs_to :giver, class_name: 'User', foreign_key: 'giver_id'
+  belongs_to :taker, class_name: 'User', foreign_key: 'taker_id'
+  belongs_to :wallet
+  belongs_to :statement
+  has_one :message
 end
