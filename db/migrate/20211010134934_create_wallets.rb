26a8a767cf279682c237caf8e3dbef346256c79f
KayKawa <goodenoughliving1221@gmail.com>
2021-10-10 22:52:29 +0900

    walletsテーブルの作成

--- db/migrate/20211010134934_create_wallets.rb
+++ db/migrate/20211010134934_create_wallets.rb
@@ -1,6 +1,8 @@
 class CreateWallets < ActiveRecord::Migration[6.1]
   def change
     create_table :wallets do |t|
+      t.references :user, foreign_key: true
+      t.integer :stock_quantity, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_10_114223) do
+ActiveRecord::Schema.define(version: 2021_10_10_134934) do
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +29,13 @@ ActiveRecord::Schema.define(version: 2021_10_10_114223) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "wallets", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "stock_quantity", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_wallets_on_user_id"
+  end
+
+  add_foreign_key "wallets", "users"
 end
