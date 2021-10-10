db6573935b235616b1363ffdbb94c97f4cc92074
KayKawa <goodenoughliving1221@gmail.com>
2021-10-10 21:43:23 +0900

    usersテーブルの作成

--- db/migrate/20211010114223_devise_create_users.rb
+++ db/migrate/20211010114223_devise_create_users.rb
@@ -4,11 +4,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: '', unique: true
+      t.string :encrypted_password, null: false, default: ''
 
+      t.string :nickname, null: false
+      t.integer :gender_id, null: false
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_kana, null: false
+      t.string :first_kana, null: false
+      t.date :birthday, null: false
       ## Recoverable
-      t.string   :reset_password_token
+      t.string   :reset_password_token, unique: true
       t.datetime :reset_password_sent_at
 
       ## Rememberable
@@ -32,12 +39,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
