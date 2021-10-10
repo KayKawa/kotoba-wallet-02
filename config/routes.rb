73498e195fb1321dfb172130aa9909e52f111813
KayKawa <goodenoughliving1221@gmail.com>
2021-10-10 23:32:56 +0900

    wallet作成の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'kotoba_wallet/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  resources :wallets, only: %i[index show] do
+    resources :purchases, only: %i[new create]
+    resources :buys, only: %i[new create]
+  end
 
   root to: 'kotoba_wallet#home'
 end
