6eec3c992b7677fc27b63a20175e3d0f6a89fde0
KayKawa <goodenoughliving1221@gmail.com>
2021-10-11 04:11:47 +0900

    buysテーブル作成

--- db/migrate/20211010191001_create_buys.rb
+++ db/migrate/20211010191001_create_buys.rb
@@ -1,6 +1,12 @@
 class CreateBuys < ActiveRecord::Migration[6.1]
   def change
     create_table :buys do |t|
+      t.references :giver, foreign_key: { to_table: :users }
+      t.references :taker, foreign_key: { to_table: :users }
+      t.references :wallet, foreign_key: true
+      t.references :statement, foreign_key: true
+      t.integer :quantity, null: false
+      t.integer :unit_price, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_10_154534) do
+ActiveRecord::Schema.define(version: 2021_10_10_191001) do
+
+  create_table "buys", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "giver_id"
+    t.bigint "taker_id"
+    t.bigint "wallet_id"
+    t.bigint "statement_id"
+    t.integer "quantity", null: false
+    t.integer "unit_price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["giver_id"], name: "index_buys_on_giver_id"
+    t.index ["statement_id"], name: "index_buys_on_statement_id"
+    t.index ["taker_id"], name: "index_buys_on_taker_id"
+    t.index ["wallet_id"], name: "index_buys_on_wallet_id"
+  end
 
   create_table "purchases", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "wallet_id"
@@ -56,6 +71,10 @@ ActiveRecord::Schema.define(version: 2021_10_10_154534) do
     t.index ["user_id"], name: "index_wallets_on_user_id"
   end
 
+  add_foreign_key "buys", "statements"
+  add_foreign_key "buys", "users", column: "giver_id"
+  add_foreign_key "buys", "users", column: "taker_id"
+  add_foreign_key "buys", "wallets"
   add_foreign_key "purchases", "statements"
   add_foreign_key "purchases", "wallets"
   add_foreign_key "statements", "wallets"
