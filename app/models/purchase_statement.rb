cc143d0798db0281a1b8d31ed6b59d828beea023
KayKawa <goodenoughliving1221@gmail.com>
2021-10-11 00:47:28 +0900

    PurchaseStatementのformObjectの生成

---
+++ app/models/purchase_statement.rb
@@ -0,0 +1,16 @@
+# Formオブジェクト
+class PurchaseStatement
+  include ActiveModel::Model
+  attr_accessor :wallet_id, :statement_id, :quantity, :plus, :stock_quantity
+
+  def save
+    # 通帳を保存
+    statement = Statement.create(plus: quantity, wallet_id: wallet_id)
+    # 発行情報を保存
+    Purchase.create(wallet_id: wallet_id, statement_id: statement.id, quantity: quantity)
+    # WALLET残高の更新
+    wallet = Wallet.find(wallet_id)
+    update_sum = wallet.stock_quantity.to_i + quantity.to_i
+    wallet.update_column(:stock_quantity, update_sum.to_i)
+  end
+end
