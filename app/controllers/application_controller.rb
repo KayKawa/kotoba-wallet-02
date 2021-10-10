f33a95525c27c624f7e3eefd1e96374d07393cdf
KayKawa <goodenoughliving1221@gmail.com>
2021-10-10 22:46:17 +0900

    ユーザー管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  # def after_sign_in_path_for(_resource)
+  #  wallets_path
+  # end
+
+  def configure_permitted_parameters
+    # 新規登録時に許可するストロングパラメータ
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :gender_id, :last_name, :first_name, :last_kana, :first_kana,
+                                             :birthday, { wallet_attributes: [:stock_quantity] }])
+    # ログイン時に許可するストロングパラメータ
+    devise_parameter_sanitizer.permit(:sign_in,
+                                      keys: [:nickname, :gender_id, :last_name, :first_name, :last_kana, :first_kana,
+                                             :birthday, { wallet_attributes: [:stock_quantity] }])
+    # ユーザー情報更新時に許可するストロングパラメータ
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: [:nickname, :gender_id, :last_name, :first_name, :last_kana, :first_kana,
+                                             :birthday, { wallet_attributes: [:stock_quantity] }])
+  end
 end
