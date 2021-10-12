ab7824c587faf1b346999a91023fb4634c8c74c9
KayKawa <goodenoughliving1221@gmail.com>
2021-10-12 19:51:43 +0900

    一覧・詳細ページに個別データを渡す（GIVE/TAKE）

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,11 +1,22 @@
 class WalletsController < ApplicationController
   def index
     @wallets = Wallet.all
+    @buys = Buy.all
+    
   end
 
   def show
     @wallet = Wallet.find(params[:id])
-    @find_give = @wallet
+
+    # これまでのgive
+    give = Buy.where(giver_id: [@wallet.user_id])
+    @give_count = give.count
+    @give_quant = give.sum(:quantity)
+
+    # これまでのtake
+    take = Buy.where(taker_id: [@wallet.user_id])
+    @take_count = take.count
+    @take_quant = take.sum(:quantity)
   end
 end
 
